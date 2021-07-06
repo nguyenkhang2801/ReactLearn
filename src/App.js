08106edbad0e8f9a1e82a13fb434154e2f3b5d26
Khang Doan <johnnydoan123@gmail.com>
2021-07-06 14:47:05 +0700

    exe2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'heehee' },
+    { id: 2, title: 'kakaka' },
+    { id: 3, title: 'hihihi' },
+  ]);
+
+  function handleTodoList(todo) {
+    const idx = todoList.findIndex(x => x.id === todo.id);
+    if (idx < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(idx, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    }
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <ColorBox />
-
+      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoList} />
     </div>
   );
 }
